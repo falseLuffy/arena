d34bd56f62e08a89debad77f6099415c506891a7
falseLuffy <falseLuffy@qq.com>
2018-08-03 09:29:04 +0800

    主页背景图片后端渲染

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 let express = require('express');
 let router = express.Router();
-let markdown = require('markdown').markdown
-const ObjectID = require('mongodb').ObjectID
-
+let markdown = require('markdown').markdown;
+const ObjectID = require('mongodb').ObjectID;
+const path = require('path');
+const  fs = require('fs');
 
 let findAll = require('../mongodb/findAll');
 let find = require('../mongodb/find')
 
+let backImage = fs.readFileSync(path.join(__dirname ,'../public/images/backImage.jpg'))
 
 /* GET home page. */
 router = router.get('/', function (req, res, next) {
    findAll().then((val) => {
-      res.render('index', {val});
+      res.render('index', {val,backImage:'data:image/jpg;base64,'+backImage.toString("base64")});
    })
 });
 router= router.get('/blog',function (req, res) {
